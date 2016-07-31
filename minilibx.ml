f6daf4588d2852ee8bc49a0dbb91b9d419bff7ce
Danny Willems <contact@danny-willems.be>
2016-07-31 18:36:49 +0200

    Update

--- minilibx.ml
+++ minilibx.ml
@@ -13,6 +13,31 @@ module Core =
       foreign "mlx_init" (void @-> returning t)
   end
 
+module Color =
+  struct
+    type t =
+      {
+        r : int;
+        g : int;
+        b : int
+      }
+
+    let of_rgb r g b = {r = r; g = g; b = b}
+
+    (* TODO *)
+    let to_int x = x.r
+
+    let red x = x.r
+    let green x = x.g
+    let blue x = x.b
+  end
+
+module Event =
+  struct
+    let loop =
+      foreign "mlx_loop" (Core.t @-> returning void)
+  end
+
 module Window =
   struct
     type t = unit ptr
@@ -29,8 +54,10 @@ module Window =
         "mlx_clear_window"
         (t @-> Core.t @-> returning int)
 
-    let loop =
-      foreign "mlx_loop" (Core.t @-> returning void)
+    let destroy =
+      foreign
+        "mlx_destroy_window"
+        (t @-> Core.t @-> returning void)
   end
 
 module Pixel =
@@ -38,13 +65,35 @@ module Pixel =
     let put =
       foreign
         "mlx_pixel_put"
-        (Core.t @-> Window.t @-> int @-> int @-> returning int)
+        (Core.t @-> Window.t @-> int @-> int @-> int @-> returning int)
+
+    let put_string =
+      foreign
+        "mlx_string_put"
+        (Core.t @-> Window.t @-> int @-> int @-> int @-> returning int)
+
+    let get_color_value_internal =
+      foreign
+        "mlx_get_color_value"
+        (Core.t @-> int @-> returning uint)
+
+    let get_color_value ptr color =
+        get_color_value_internal ptr (Color.to_int color)
   end
-(*
-module Color =
+
+module Image =
   struct
-    type t = int
-    let t_of_int x = x
-    let int_to_t x = x
+    type t = unit ptr
+
+    let t : t typ = ptr void
+
+    let new_ =
+      foreign
+        "mlx_new_image"
+        (Core.t @-> int @-> int @-> returning t)
+
+    let put_to_window =
+      foreign
+        "mlx_put_image_to_window"
+        (Core.t @-> Window.t @-> t @-> int @-> int @-> returning int)
   end
-*)
--- minilibx.mli
+++ minilibx.mli
@@ -1,3 +1,16 @@
+module Color :
+  sig
+    type t
+
+    val of_rgb : int -> int -> int -> t
+
+    val to_int : t -> int
+
+    val red : t -> int
+    val green : t -> int
+    val blue : t -> int
+  end
+
 module Core :
   sig
     type t
@@ -17,10 +30,40 @@ module Window :
 
     val clear : t -> Core.t -> int
 
+    val destroy : t -> Core.t -> unit
+  end
+
+module Event :
+  sig
     val loop : Core.t -> unit
   end
 
 module Pixel :
   sig
-    val put : Core.t -> Window.t -> int -> int -> int
+    val put : Core.t -> Window.t -> int -> int -> int -> int
+
+    val put_string : Core.t -> Window.t -> int -> int -> int -> int
+
+    val get_color_value : Core.t -> Color.t -> Unsigned.uint
+  end
+
+module Image :
+  sig
+    type t
+
+    val t             : t Ctypes.typ
+
+    val new_          :
+      Core.t  ->
+      int     ->
+      int     ->
+      t
+
+    val put_to_window :
+      Core.t    ->
+      Window.t  ->
+      t         ->
+      int       ->
+      int       ->
+      int
   end
